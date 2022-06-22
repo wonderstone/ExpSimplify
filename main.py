5ab305889b32e57dae6696c39e33f05e2bedab35
wonderstone <tieqiw@hotmail.com>
2022-06-22 20:08:32 +0800

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -4,7 +4,11 @@
 from sympy import *
 
 if __name__ == '__main__':
+    # the comments below is another way to do the simplify job by compile
+    # codeObj = compile("a = Symbol('a')", '', 'exec')
+    # exec(codeObj)
     a = Symbol('a')
+
     expr = (((a * a) + (a / a)) - ((a - a) - a)) + \
            (((a * a) * a) * a) + \
            (((a / a) / a) - (a / a)) + \
