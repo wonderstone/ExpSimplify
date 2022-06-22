8b36f7bf262f82e072d3e5c0734ae0e3b5e57740
wonderstone <tieqiw@hotmail.com>
2022-06-22 10:54:29 +0800

    minor adjustment

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+# -*- coding: UTF-8 -*-
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# import the SymPy library
+from sympy import *
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    a = Symbol('a')
+    expr = (((a * a) + (a / a)) - ((a - a) - a))+(((a * a) * a) * a)+(((a / a) / a) - (a / a))+((a * (a * a)) - ((a / a) / a))
+    print(simplify(expr))
+    print(factor(expr))
+    print(expand(expr))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
